f89168e5ff33dc35a9136a1ace786e08d43a7a42
= <juan.marulanda9811@gmail.com>
2022-10-11 11:43:30 -0500

    Comunication amoung services with feing and hystrix

--- shopping-service/src/main/java/academy/digitallab/store/shopping/entity/Invoice.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/entity/Invoice.java
@@ -1,5 +1,6 @@
 package academy.digitallab.store.shopping.entity;
 
+import academy.digitallab.store.shopping.model.Customer;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
@@ -32,8 +33,6 @@ public class Invoice {
     @Temporal(TemporalType.DATE)
     private Date createAt;
 
-
-
     @Valid
     @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
@@ -41,6 +40,9 @@ public class Invoice {
     private List<InvoiceItem> items;
 
     private String state;
+
+    @Transient
+    private Customer customer;
 
     public Invoice(){
         items = new ArrayList<>();
